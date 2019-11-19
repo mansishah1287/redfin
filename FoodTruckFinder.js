17e3b0f0d351103354d838e41364c282f3a164d3
mansi shah <mansi.shah2@coxautoinc.com>
2019-11-19 12:56:16 -0600

    Revision and improvement

--- FoodTruckFinder.js
+++ FoodTruckFinder.js
@@ -2,8 +2,14 @@ let request = require('request');
 let inquirer = require('inquirer');
 
 let getCurrentHourAndDay = () => { 
-	let currentHourAndDay = new Date().toLocaleTimeString([], { weekday: 'long', hour: '2-digit' });
-	return currentHourAndDay.split(',');
+	let currentHourAndDay = new Date().toLocaleTimeString('en-US', { hour: '2-digit', hour12: true, weekday: 'long' });
+  console.log(currentHourAndDay);
+  let timeParts = currentHourAndDay.split(',');
+  console.log(timeParts);
+  let time = timeParts[1].replace(/\s/g, '');
+  let day = timeParts[0];
+  console.log(time);
+	return [day, time];
 };
 
 //return 10 records at a time
@@ -33,17 +39,17 @@ let callPrompt = (count, foodTruckList) => {
   					}
   					])
 
-  				.then(answers => {
-  						// console.log(answers);
-  				// 		count = count + 10
-						// console.log(count);
-						if(foodTruckList.length - count <= 0) {
-							console.log("No more trucks found");
-						} else {
-							console.log(findFoodTruckRecords(foodTruckList, count).length);
-						}
+  				// .then(answers => {
+  				// 		// console.log(answers);
+  				// // 		count = count + 10
+						// // console.log(count);
+						// if(foodTruckList.length - count <= 0) {
+						// 	console.log("No more trucks found");
+						// } else {
+						// 	console.log(findFoodTruckRecords(foodTruckList, count));
+						// }
   						
-  				})
+  				// })
 }
 
 async function callConfirm(count, foodTruckList) {
@@ -58,26 +64,29 @@ async function callConfirm(count, foodTruckList) {
   				.then(answers => {
   						console.log(answers);
   						if(answers.seeNextList === true) {
-  							callPrompt(count, foodTruckList);
+  							//let answer = await callPrompt(count, foodTruckList);
+                //console.log("Answer to the callPrompt " , answer);
+                console.log(findFoodTruckRecords(foodTruckList, count));
   							count = count + 10
 							console.log(count);
-  							setTimeout(callConfirm, 5000, count, foodTruckList);
+  							//setTimeout(callConfirm, 5000, count, foodTruckList);
+               callConfirm(count, foodTruckList);
   							
   						}
   				})
 }
 
 //return all food trucks open when the program is run
-let getFoodTruckList = function() {
-	let timeParts = getCurrentHourAndDay();
-	//let time = timeParts[1].replace(/\s/g, '');
-	let time = "12PM";
-	let day = timeParts[0];
+  let getFoodTruckList = function() {
+	let [day , time] = getCurrentHourAndDay();
+	// let time = timeParts[1].replace(/\s/g, '');
+	//let time = "12PM";
+	
 	//console.log(time + " " + day);
 
 	//console.log(`https://data.sfgov.org/resource/jjew-r69b.json?DayOfWeekStr=${day}&starttime=${time}`);
 	request(`https://data.sfgov.org/resource/jjew-r69b?DayOfWeekStr=${day}&starttime=${time}`, function (error, response, body) {
-  		// console.log(body);
+  		//console.log(body);
   		if (error) {
 
   		} else if(response.status <= 204) {
@@ -85,9 +94,9 @@ let getFoodTruckList = function() {
   		} else {
   			let foodTruckList = JSON.parse(body);
   			// console.log(foodTruckList.length);
-  			foodTruckList.sort((a, b) => a.applicant.localeCompare(b.applicant));
+  			//foodTruckList.sort((a, b) => a.applicant.localeCompare(b.applicant));
   			//console.log(foodTruckList);
-  			console.log(foodTruckList.length);
+  			//console.log(foodTruckList);
   			// for(let i=0 ; i < foodTruckList.length ; i++) {
   			// 	let tenTrucksList = foodTruckList.slice(i, 9);
   			// 	console.log(tenTrucksList);
@@ -95,9 +104,7 @@ let getFoodTruckList = function() {
   			let count = 0;
   			let results = findFoodTruckRecords(foodTruckList, count);
   			//let finalTruckListKeys = ['Applicant', 'Location'];
-  			let finalTruckList = results.map(item => {
-  				return {Name : item.applicant, Address: item.location};
-  			});
+  			let finalTruckList = results.map(({applicant, location}) => ({ Name : applicant, Address: location }));
   			console.log(finalTruckList);
 
 
